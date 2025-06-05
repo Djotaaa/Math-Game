7c98be46dd56eb221a58a43bdddf4c29f424ac82
Djordje Jokic <djordjejokic14@gmail.com>
2025-06-05 18:31:01 +0200

    Add project files.

---
+++ Math Game/Games.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Sources;
+
+namespace Math_Game
+{
+    internal class Games
+    {
+        Random random = new Random();
+        internal void AdditionGame()
+        {
+            int score = 0;
+            Console.WriteLine("Welcome to Addition game!");
+            Console.WriteLine("When you are ready press any key...");
+            Console.ReadKey();
+            for(int i = 0; i < 5; i++)
+            {
+                int number1 = random.Next(1, 10);
+                int number2 = random.Next(1, 10);
+
+                Console.WriteLine($"{number1} + {number2} =");
+                string? result = Console.ReadLine();
+
+                if(Convert.ToInt32(result) == number1 + number2)
+                {
+                    score++;   
+                }
+            }
+            if(score > 3)
+            {
+                Console.WriteLine($"Congratulations, your score is {score}.");
+                Thread.Sleep(1000);
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine($"Maybe you will have better luck next time, your score is {score}.");
+                Thread.Sleep(1000);
+                Environment.Exit(0);
+            }
+        }
+
+        internal void SubstractionGame()
+        {
+            int score = 0;
+            Console.WriteLine("Welcome to Substraction game!");
+            Console.WriteLine("When you are ready press any key...");
+            Console.ReadKey();
+            for(int i = 0; i < 5; i++)
+            {
+                int number1, number2;
+                do
+                {
+                    number1 = random.Next(10, 99);
+                    number2 = random.Next(1, 50);
+                } while (number1 <= number2);
+
+                Console.WriteLine($"{number1} - {number2} =");
+                string? result = Console.ReadLine();
+
+                if (Convert.ToInt32(result) == number1 - number2)
+                {
+                    score++;
+                }
+            }
+            if(score > 3)
+            {
+                Console.WriteLine($"Congratulations, your score is {score}.");
+                Thread.Sleep(1000);
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine($"Maybe you will have better luck next time, your score is {score}.");
+                Thread.Sleep(1000);
+                Environment.Exit(0);
+            }
+        }
+
+        internal void MultiplicationGame()
+        {
+            int score = 0;
+            Console.WriteLine("Welcome to Multiplication game!");
+            Console.WriteLine("When you are ready press any key...");
+            Console.ReadKey();
+            for (int i = 0; i < 5; i++)
+            {
+                int number1 = random.Next(1, 10);
+                int number2 = random.Next(1, 10);
+
+                Console.WriteLine($"{number1} * {number2} =");
+                string? result = Console.ReadLine();
+
+                if (Convert.ToInt32(result) == number1 * number2)
+                {
+                    score++;
+                }
+            }
+            if(score > 3)
+            {
+                Console.WriteLine($"Congratulations, your score is {score}.");
+                Thread.Sleep(1000);
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine($"Maybe you will have better luck next time, your score is {score}.");
+                Thread.Sleep(1000);
+                Environment.Exit(0);
+            }
+        }
+
+        internal void DivisionGame()
+        {
+            int score = 0;
+            Console.WriteLine("Welcome to Division game!");
+            Console.WriteLine("When you are ready press any key...");
+            Console.ReadKey();
+            for(int i = 0; i < 5; i++)
+            {
+                int number1, number2;
+                do
+                {
+                    number1 = random.Next(1, 99);
+                    number2 = random.Next(1, 10);
+                } while (number1 % number2 != 0);
+
+                Console.WriteLine($"{number1} / {number2} =");
+                string? result = Console.ReadLine();
+
+                if (Convert.ToInt32(result) == number1 / number2)
+                {
+                    score++;
+                }
+            }
+
+            if (score > 3)
+            {
+                Console.WriteLine($"Congratulations, your score is {score}.");
+                Thread.Sleep(1000);
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine($"Maybe you will have better luck next time, your score is {score}.");
+                Thread.Sleep(1000);
+                Environment.Exit(0);
+            }
+        }
+    }
+}
---
+++ Math Game/Program.cs
@@ -0,0 +1,55 @@
+﻿using Math_Game;
+
+Console.WriteLine("Welcome to Math Game. Can you tell us what's your name ?");
+string? name = Console.ReadLine();
+DateTime date = DateTime.Now;
+Games games = new Games();
+
+while (name.Trim() == "")
+{
+    Console.WriteLine("It's necessary to input your name, try again:");
+    name = Console.ReadLine();
+}
+Console.WriteLine("--------------------------------------------------------");
+Console.WriteLine($"Hello {name.ToUpper()}! It's {date.DayOfWeek}. What kind of game you want to play ?");
+Console.WriteLine("  Addition - press A");
+Console.WriteLine("  Substraction - press S");
+Console.WriteLine("  Multiplication - press M");
+Console.WriteLine("  Division - press D");
+Console.WriteLine("If you want to exit the game - press Q");
+Console.WriteLine("--------------------------------------------------------");
+
+string? userInput = Console.ReadLine();
+
+while (userInput == "" || userInput == null)
+{
+    Console.WriteLine("You need to choose game type or exit the game. Try again:");
+    userInput = Console.ReadLine();
+}
+
+switch (userInput.ToLower().Trim())
+{
+    case "a":
+        games.AdditionGame();
+        break;
+    case "s":
+        games.SubstractionGame();
+        break;
+    case "m":
+        games.MultiplicationGame();
+        break;
+    case "d":
+        games.DivisionGame();
+        break;
+    case "q":
+        Console.WriteLine($"Good bye {name.ToUpper()} !!!");
+        Thread.Sleep(1000);
+        Environment.Exit(0);
+    break;
+    default:
+        Console.WriteLine("Invalid input!");
+        Thread.Sleep(1000);
+        Environment.Exit(1);
+        break;
+}
+
